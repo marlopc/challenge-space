c27526937eef1bd74cbd4f6a3c6c3483c7693db7
marlopc <lucaspanaro1@gmail.com>
2022-05-03 05:14:47 -0300

    refactor: changes a media query variable name

--- styles/queries.ts
+++ styles/queries.ts
@@ -1,4 +1,4 @@
-const screen2KOnly = `@media (min-width: 1440px)`;
+const above2K = `@media (min-width: 1440px)`;
 const desktopOnly = `@media (min-width: 900px)`;
 const tabletOnly = `@media (min-width: 650px) and (max-width: 899px)`;
 const mobileOnly = `@media (max-width: 649px)`;
@@ -6,7 +6,7 @@ const tabletAndDesktop = `@media (min-width: 650px)`;
 const mobileAndDesktop = `@media (max-width: 649px), (min-width: 900px)`;
 
 export {
-  screen2KOnly,
+  above2K,
   desktopOnly,
   tabletOnly,
   mobileOnly,
