04bae672f846079e7091219509b85a3061fae4aa
marlopc <lucaspanaro1@gmail.com>
2022-05-03 05:25:00 -0300

    feat: adds padding to background container

--- containers/Background/Background.tsx
+++ containers/Background/Background.tsx
@@ -4,17 +4,20 @@ import { desktopOnly, tabletOnly } from "styles/queries";
 export default styled.div<{ page: keyof DefaultTheme["background"] }>`
   min-height: 100vh;
   width: 100%;
+  padding-top: 85px;
   background: no-repeat center;
   background-size: cover;
   background-image: ${(props) =>
     `url(${props.theme.background[props.page].mobile})`};
 
   ${tabletOnly} {
+    padding-top: 100px;
     background-image: ${(props) =>
       `url(${props.theme.background[props.page].tablet})`};
   }
 
   ${desktopOnly} {
+    padding-top: 160px;
     background-image: ${(props) =>
       `url(${props.theme.background[props.page].desktop})`};
   }
